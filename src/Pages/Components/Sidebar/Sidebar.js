49e7155bcc81d395be54ba5c3c25f8f6f4067035
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-12-10 22:04:12 +0600

    Sidebar Done

--- src/Pages/Components/Sidebar/Sidebar.js
+++ src/Pages/Components/Sidebar/Sidebar.js
@@ -13,8 +13,9 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import Logo from '../../../img/logo.png'
 
-const drawerWidth = 240;
+const drawerWidth = 350;
 
 const Sidebar = () => {
   return (
@@ -24,6 +25,7 @@ const Sidebar = () => {
       <Drawer
         PaperProps={{
           sx: {
+            paddingLeft: "20px",
             backgroundColor: "#F0F6FF",
             width: drawerWidth,
             flexShrink: 0,
@@ -36,22 +38,69 @@ const Sidebar = () => {
         variant="permanent"
         anchor="left"
       >
+        <img src={Logo} className="w-52 mt-12 pl-6" alt="" />
         <Toolbar />
-        <List className="">
-          {["Overview", "Product", "Customer", "Orders"].map((text, index) => (
-            <ListItem
-              className=" hover:bg-drawerHover hover:rounded-full"
-              key={text}
-              disablePadding
-            >
-              <ListItemButton>
-                <ListItemIcon>
-                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+        <List>
+          <ListItem className="hover:bg-drawerHover hover:rounded-l-full mb-2 p-4">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-8 h-6 mr-2 "
+              >
+                <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
+              </svg>
+            </div>
+            <p className="font-semibold">Overview</p>
+          </ListItem>
+          <ListItem className="hover:bg-drawerHover hover:rounded-l-full mb-2 p-4">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-8 h-7 mr-2"
+              >
+                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+              </svg>
+            </div>
+            <p className="font-semibold">Products</p>
+          </ListItem>
+          <ListItem className="hover:bg-drawerHover hover:rounded-l-full mb-2 p-4">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-8 h-6 mr-2"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <p className="font-semibold">Customers</p>
+          </ListItem>
+          <ListItem className="hover:bg-drawerHover hover:rounded-l-full mb-2 p-4">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-8 h-7 mr-2"
+              > 
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <p className="font-semibold">Orders</p>
+          </ListItem>
         </List>
       </Drawer>
       {/* </Box> */}
